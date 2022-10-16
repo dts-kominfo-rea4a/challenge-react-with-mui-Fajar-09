06a122371bf57d98bab84368581b87109c832bd6
Fajar-09 <fajarrh2002@gmail.com>
2022-10-16 14:08:51 +0700

    Push ke-2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import "./App.css";
+import React, { useState } from "react";
 
 import Header from "./components/Header";
+import ContactForm from "./components/ContactForm";
+import Contact from "./components/Contact";
+
+// mengambil data JSON dari contact.json
+import dataJSON from "./data/contacts.json";
+
+import { Grid } from "@mui/material";
 
 // Uncomment untuk memuat daftar kontak
 // import contactsJSON from './data/contacts.json';
@@ -11,10 +19,25 @@ const App = () => {
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+  const [contact, setContact] = useState(dataJSON);
 
+  // fungsi untuk menambah contact
+  function tambahContact(objectBaru) {
+    // menambahkan data array yang baru
+    setContact([...contact, objectBaru]);
+  }
   return (
     <div className="App">
       <Header />
+
+      <Grid container spacing={2}>
+        <Grid item xs={5}>
+          <ContactForm propsAddContact={tambahContact} />
+        </Grid>
+        <Grid item xs={6}>
+          <Contact data={contact} />
+        </Grid>
+      </Grid>
     </div>
   );
 };
